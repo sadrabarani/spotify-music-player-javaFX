0b09907199c8d61ca7920418cf1f01b31c598f3c
sadrabarani <sadra86barani@gmail.com>
2024-05-06 00:30:50 +0330

    login in and sign up fxml

---
+++ firstProjPhase2/src/main/java/GUI/mainSection/Login.java
@@ -0,0 +1,37 @@
+package GUI.mainSection;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private ComboBox<?> kindUserCombo;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private Label loginLbl;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    private Label passlbl;
+
+    @FXML
+    private Label userLbl;
+
+    @FXML
+    private TextField userNameTxt;
+
+}
+
---
+++ firstProjPhase2/src/main/java/GUI/mainSection/SignInPg.java
@@ -0,0 +1,35 @@
+package GUI.mainSection;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class SignInPg {
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private DatePicker dateBirth;
+
+    @FXML
+    private TextField emailTxt;
+
+    @FXML
+    private TextField fulNameTxt;
+
+    @FXML
+    private TextField passTxt;
+
+    @FXML
+    private TextField phoneNimberTxt;
+
+    @FXML
+    private Button signInBtn;
+
+    @FXML
+    private TextField userNameTxt;
+
+}
+
