a988f6dad3f75393c9eb5890b63b07294c404458
sadrabarani <sadra86barani@gmail.com>
2024-05-06 16:19:30 +0330

    listener panel fxml fav_Genre.fxml

---
+++ firstProjPhase2/src/main/java/GUI/mainSection/FavGenre.java
@@ -0,0 +1,43 @@
+package GUI.mainSection;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+
+public class FavGenre {
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private CheckBox country;
+
+    @FXML
+    private CheckBox hiphop;
+
+    @FXML
+    private CheckBox history;
+
+    @FXML
+    private CheckBox interview;
+
+    @FXML
+    private CheckBox jazz;
+
+    @FXML
+    private CheckBox pop;
+
+    @FXML
+    private CheckBox rock;
+
+    @FXML
+    private CheckBox society;
+
+    @FXML
+    private Button submitBtn;
+
+    @FXML
+    private CheckBox trueCrime;
+
+}
+
---
+++ firstProjPhase2/src/main/java/GUI/mainSection/ListenerPanel.java
@@ -0,0 +1,58 @@
+package GUI.mainSection;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class ListenerPanel {
+
+    @FXML
+    private Button BackBtn;
+
+    @FXML
+    private Button BackBtn1;
+
+    @FXML
+    private Button BackBtn4;
+
+    @FXML
+    private Button addPlayList;
+
+    @FXML
+    private Label creditLbl;
+
+    @FXML
+    private Label dateBirth;
+
+    @FXML
+    private Label emaillbl;
+
+    @FXML
+    private ListView<?> folowingsListView1;
+
+    @FXML
+    private Label nameLbl;
+
+    @FXML
+    private Button oneMonSubBtn;
+
+    @FXML
+    private Label passlbl;
+
+    @FXML
+    private Label phoneLbl;
+
+    @FXML
+    private ListView<?> playListView;
+
+    @FXML
+    private Button sixMonSubBtn;
+
+    @FXML
+    private Button twoMonSubBtn;
+
+    @FXML
+    private Label userlbl;
+
+}
