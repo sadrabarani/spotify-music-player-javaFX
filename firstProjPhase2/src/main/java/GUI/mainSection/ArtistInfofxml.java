78b1b2aa401809d0a580245459aa419a5bf11a33
sadrabarani <sadra86barani@gmail.com>
2024-05-07 16:50:05 +0330

    artist_infofxml.fxml SetMainScene

---
+++ firstProjPhase2/src/main/java/GUI/mainSection/ArtistInfofxml.java
@@ -0,0 +1,30 @@
+package GUI.mainSection;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+public class ArtistInfofxml {
+
+    @FXML
+    private Button FollowBtn;
+
+    @FXML
+    private Label artistInfoLbl;
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private ListView<?> listMusicOfArtist;
+
+    @FXML
+    private TextArea reportDis;
+
+    @FXML
+    private Button subReport;
+
+}
+
