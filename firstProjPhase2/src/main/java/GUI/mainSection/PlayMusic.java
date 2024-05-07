f431a8eab16dc7ab090d7582a12fe8a609c09125
sadrabarani <sadra86barani@gmail.com>
2024-05-07 11:54:33 +0330

    play Music

---
+++ firstProjPhase2/src/main/java/GUI/mainSection/PlayMusic.java
@@ -0,0 +1,38 @@
+package GUI.mainSection;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.shape.Rectangle;
+
+public class PlayMusic {
+
+    @FXML
+    private MenuButton addPlayList;
+
+    @FXML
+    private Label artistLbl;
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private Rectangle cover;
+
+    @FXML
+    private Label genreLbl;
+
+    @FXML
+    private Button likeBtn;
+
+    @FXML
+    private Label lyricLbl;
+
+    @FXML
+    private Label musicNameLbl;
+
+    @FXML
+    private Label numerOfLikesLbl;
+
+}
