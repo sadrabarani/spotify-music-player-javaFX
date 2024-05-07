f8ba7cd8867402b23b9e20375cead3fd1f8335c8
sadrabarani <sadra86barani@gmail.com>
2024-05-08 00:52:13 +0330

    all artist_infofxml.fxml

--- firstProjPhase2/src/main/java/GUI/mainSection/ArtistInfofxml.java
+++ firstProjPhase2/src/main/java/GUI/mainSection/ArtistInfofxml.java
@@ -1,13 +1,20 @@
 package GUI.mainSection;
 
+import GUI.SetMainScene;
+import controller.ListenerControler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import model.UserAccount.Artist;
 
-public class ArtistInfofxml {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ArtistInfofxml implements Initializable {
     public static Artist artist;
 
     public static Artist getArtist() {
@@ -19,7 +26,7 @@ public class ArtistInfofxml {
     }
 
     @FXML
-    private Button FollowBtn;
+    private Button followBtn;
 
     @FXML
     private Label artistInfoLbl;
@@ -28,7 +35,7 @@ public class ArtistInfofxml {
     private Button backBtn;
 
     @FXML
-    private ListView<?> listMusicOfArtist;
+    private ListView listMusicOfArtist;
 
     @FXML
     private TextArea reportDis;
@@ -36,5 +43,26 @@ public class ArtistInfofxml {
     @FXML
     private Button subReport;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        subReport.setOnMouseClicked(e->{
+            if(reportDis.getText().isEmpty()){
+
+            }else{
+                ListenerControler.getListenerControler().reportArtist(artist.getUsername(),reportDis.getText());
+            }
+        });
+        artistInfoLbl.setText(artist.getFullName()+"\n"+artist.getUsername()+"\n"+artist.getFollowers()+"\n"+artist.getBiography()+"\n"+artist.getEmail());
+        followBtn.setOnMouseClicked(e->{
+            ListenerControler.getListenerControler().followArtist(artist.getUsername());
+        });
+        backBtn.setOnMouseClicked(e->{
+            try {
+                SetMainScene.setScene(10);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+    }
 }
 
