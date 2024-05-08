cddf1a38394cd19e09db39a8f9497688c16fb068
sadrabarani <sadra86barani@gmail.com>
2024-05-09 00:27:10 +0330

    making popup

--- firstProjPhase2/src/main/java/GUI/SideBar.java
+++ firstProjPhase2/src/main/java/GUI/SideBar.java
@@ -63,57 +63,57 @@ public class SideBar implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        searchField.setOnKeyPressed(e->{
-            if(e.getCode()== KeyCode.ENTER){
-                if(searchField!=null) {
-                    SearchPage.setArrayList(ListenerControler.getListenerControler().searchByArtistName(searchField.getText()));
-                    SearchPage.setAudioArrayList(ListenerControler.getListenerControler().searchByAudio(searchField.getText()));
-                }
-            }
-        });
-        libraryBtn.setOnMouseClicked(e->{
-            if()//todo not login
-            {
-
-            }
-            else {
-                try {
-                    SetMainScene.setScene(1);
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        });
-        homeBtn.setOnMouseClicked(e1->{
-            if (){
-                try {
-                    SetMainScene.setScene(9);
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }//todo not login
-            else //todo logined
-            {
-                try {
-                    SetMainScene.setScene(10);
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        });
-        audiosBtn.setOnMouseClicked(mouseEvent -> {
-            try {
-                SetMainScene.setScene(6);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        artistBtn.setOnMouseClicked(mouseEvent -> {
-            try {
-                SetMainScene.setScene(7);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+//        searchField.setOnKeyPressed(e->{
+//            if(e.getCode()== KeyCode.ENTER){
+//                if(searchField!=null) {
+//                    SearchPage.setArrayList(ListenerControler.getListenerControler().searchByArtistName(searchField.getText()));
+//                    SearchPage.setAudioArrayList(ListenerControler.getListenerControler().searchByAudio(searchField.getText()));
+//                }
+//            }
+//        });
+//        libraryBtn.setOnMouseClicked(e->{
+//            if()//todo not login
+//            {
+//
+//            }
+//            else {
+//                try {
+//                    SetMainScene.setScene(1);
+//                } catch (IOException ex) {
+//                    throw new RuntimeException(ex);
+//                }
+//            }
+//        });
+//        homeBtn.setOnMouseClicked(e1->{
+//            if (){
+//                try {
+//                    SetMainScene.setScene(9);
+//                } catch (IOException ex) {
+//                    throw new RuntimeException(ex);
+//                }
+//            }//todo not login
+//            else //todo logined
+//            {
+//                try {
+//                    SetMainScene.setScene(10);
+//                } catch (IOException ex) {
+//                    throw new RuntimeException(ex);
+//                }
+//            }
+//        });
+//        audiosBtn.setOnMouseClicked(mouseEvent -> {
+//            try {
+//                SetMainScene.setScene(6);
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
+//        artistBtn.setOnMouseClicked(mouseEvent -> {
+//            try {
+//                SetMainScene.setScene(7);
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
     }
 }
