dd16c8b9b220d3c34db769b4b61621efe10d78af
sadrabarani <sadra86barani@gmail.com>
2024-05-11 23:35:25 +0330

    SuccesPopUp

---
+++ firstProjPhase2/src/main/java/GUI/SuccesPopUp.java
@@ -0,0 +1,32 @@
+package GUI;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class SuccesPopUp {
+    public static void showSuccessfulMessage(Stage stage1) {
+        Stage primaryStage= stage1;
+        Popup popup = new Popup();
+        Pane pane = new Pane();
+        popup.getContent().add(pane);
+        double x = primaryStage.getX()+10;
+        double y = primaryStage.getY()+600;
+        Button button=new Button("Succesfull");
+        button.setStyle("-fx-background-color: green");
+        pane.getChildren().addAll(button);
+        popup.setX(x);
+        popup.setY(y);
+        popup.show(primaryStage);
+        Duration delay = Duration.seconds(3);
+        Timeline timeline = new Timeline(new KeyFrame(delay, event -> popup.hide()));
+        timeline.play();
+    }
+}
